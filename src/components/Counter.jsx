234d9c2801e251a8501432bd79ad8a14a8c64310
emmanuelOjo <emmanuelojo1234@gmail.com>
2022-07-14 00:00:57 +0100

    First commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const Counter = () => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <button> Increment </button>
+    </div>
+  );
+};
+
+export default Counter;
